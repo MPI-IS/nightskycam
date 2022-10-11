8a8d3a3b8d69d920564817f48d3cdad0d0bb5eaa
Vincent Berenz <vberenz@tuebingen.mpg.de>
2022-10-11 06:25:20 +0200

    updating config file from remote: using lock

--- nightskycam/configuration_file.py
+++ nightskycam/configuration_file.py
@@ -2,6 +2,7 @@ import logging
 import importlib
 import typing
 import toml
+from .locks import Locks
 from pathlib import Path
 from .skythread import SkyThread
 from .types import GlobalConfiguration
@@ -191,19 +192,20 @@ def local_config_cleanup(folder: Path, main_file: str) -> None:
     deleted the other, and setup 'nightskycam.toml' to be a symlink to this
     file.
     """
-    local_files = list_local_config_files(folder)
-    best_file = best_config_file(local_files)
-    main_path = folder / main_file
-    try:
-        main_path.unlink()
-    except Exception:
-        pass
-    main_path.symlink_to(best_file)
-    to_delete = [
-        folder / filename for filename in local_files if not filename == best_file
-    ]
-    for path in to_delete:
-        path.unlink()
+    with Locks.get_lock("configuration"):
+        local_files = list_local_config_files(folder)
+        best_file = best_config_file(local_files)
+        main_path = folder / main_file
+        try:
+            main_path.unlink()
+        except Exception:
+            pass
+        main_path.symlink_to(best_file)
+        to_delete = [
+            folder / filename for filename in local_files if not filename == best_file
+        ]
+        for path in to_delete:
+            path.unlink()
 
 
 def current_config_file() -> str:
